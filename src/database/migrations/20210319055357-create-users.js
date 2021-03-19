3a6d5834dfc5760ac4a17ab7a42a5e80eba56b85
lucasjunior24 <lukateku@gmail.com>
2021-03-19 03:14:54 -0300

    Rodando primeira migrate

--- src/database/migrations/20210319055357-create-users.js
+++ src/database/migrations/20210319055357-create-users.js
@@ -27,6 +27,14 @@ module.exports = {
         allowNull: false,
         defaultValue: false,
       },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
     });
   },
 
