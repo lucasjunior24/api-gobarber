2fa2d114e3fbb64adc8ff5da893b519c091cbb44
lucasjunior24 <lukateku@gmail.com>
2021-03-19 23:33:27 -0300

    Criando loader do model User

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -2,15 +2,17 @@ import Sequelize, { Model } from 'sequelize'
 
 class User extends Model {
     static init(sequelize) {
-        super.init({
-            name: Sequelize.STRING,
-            email: Sequelize.STRING,
-            password_hash: Sequelize.STRING,
-            provider: Sequelize.BOOLEAN
-        },
-        {
-            Sequelize,
-        })
+        super.init(
+            {
+                name: Sequelize.STRING,
+                email: Sequelize.STRING,
+                password_hash: Sequelize.STRING,
+                provider: Sequelize.BOOLEAN,
+            },
+            {
+                sequelize,
+            }
+        )
     }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,18 @@
 import { Router } from 'express'
 
+import User from './app/models/User'
 
 const routes = new Router()
 
-routes.get('/', (req, res) => {
-    return res.json({ message: 'Hello Lucas' })
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'Lucas Junior',
+        email: 'lucas@gmail.com',
+        password_hash: '12345',
+    })
+    return res.json(user)
+
+    // return res.json({ message: 'Hello Lucas' })
 
 })
 
