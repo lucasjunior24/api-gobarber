b42c67655732d3d8199d74482ff5d4287165890f
lucasjunior24 <lukateku@gmail.com>
2021-03-21 21:34:10 -0300

    Listagem de provedores de serviço

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
-import routes from './routes';
+import path from 'path'
 
+import routes from './routes';
 import './database/index'
 
 class App {
@@ -13,6 +14,7 @@ class App {
 
     middlewares() {
         this.server.use(express.json());
+        this.server.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')))
     }
 
     routes() {
