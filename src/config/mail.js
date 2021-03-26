5dda51fd7f2fc9441518e93508b2de7db5b452b6
lucasjunior24 <lukateku@gmail.com>
2021-03-26 15:06:25 -0300

    Configurando Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,14 @@
+export default {
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    secure: false,
+    auth: {
+        user: "c61beb2c02c084",
+        pass: "ffa2c7b90b1199"
+    },
+    default: {
+        from: 'Equipe goBarber <noreply@gobarber.com>',
+    },
+};
+
+
